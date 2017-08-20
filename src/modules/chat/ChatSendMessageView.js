871593f07780778bb62f4280d9f7a18b48a86c17
Vlad Aituganov <vlad.aituganov@gmail.com>
2017-08-20 16:06:27 +0300

    Extracted ChatSendMessageView

---
+++ src/modules/chat/ChatSendMessageView.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import TextArea from '../../components/TextArea';
+import Button from '../../components/Button';
+
+import s from './ChatSendMessageView.css';
+
+
+export default class ChatSendMessageView extends React.PureComponent {
+  state = {
+    chatMessage: ''
+  }
+
+  handleChatMessageChange = val => {
+    this.setState({ chatMessage: val });
+  }
+
+  handleTextAreaKeyDown = ev => {
+    if ((ev.keyCode === 13) && ev.ctrlKey) {
+      this.sendChatMessage();
+    }
+  }
+
+  sendChatMessage = () => {
+    this.props.onSendMessage(this.state.chatMessage);
+    this.setState({ chatMessage: '' });
+  }
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.sendContainer}>
+          <h1 className={s.heading}>Чат</h1>
+
+          <TextArea
+            className={s.textArea}
+            placeholder="Сообщение"
+            value={this.state.chatMessage}
+            onChange={this.handleChatMessageChange}
+            onKeyDown={this.handleTextAreaKeyDown}
+          />
+          <Button
+            className={s.sendButton}
+            onClick={this.sendChatMessage}
+          >
+            Отправить
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/modules/chat/ChatView.js
+++ src/modules/chat/ChatView.js
@@ -1,53 +1,16 @@
 import React from 'react';
-import TextArea from '../../components/TextArea';
-import Button from '../../components/Button';
-
-import s from './ChatView.css';
+import ChatSendMessageView from './ChatSendMessageView';
 
 const debug = require('debug')('rocket:chat-view');
 
 
-export default class Chat extends React.PureComponent {
-  state = {
-    chatMessage: ''
-  }
-
-  handleChatMessageChange = val => {
-    this.setState({ chatMessage: val });
-  }
-
-  handleTextAreaKeyDown = ev => {
-    if ((ev.keyCode === 13) && ev.ctrlKey) {
-      this.sendChatMessage();
-    }
-  }
-
-  sendChatMessage = () => {
-    debug('yay', this.state.chatMessage);
-    this.setState({ chatMessage: '' });
-  }
-
+export default class ChatView extends React.PureComponent {
   render() {
     return (
-      <div className={s.chatView}>
-        <div className={s.sendContainer}>
-          <h1 className={s.heading}>Чат</h1>
-
-          <TextArea
-            className={s.textArea}
-            placeholder="Сообщение"
-            value={this.state.chatMessage}
-            onChange={this.handleChatMessageChange}
-            onKeyDown={this.handleTextAreaKeyDown}
-          />
-          <Button
-            className={s.sendButton}
-            onClick={this.sendChatMessage}
-          >
-            Отправить
-          </Button>
-        </div>
+      <div>
+        <ChatSendMessageView onSendMessage={msg => debug('send message', msg)} />
       </div>
     );
   }
 }
+
